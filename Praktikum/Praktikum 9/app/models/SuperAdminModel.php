d48f0729a6e1419879cbcea7810f94fd2b67561f
Winata <71538946+hamzahwiranataa@users.noreply.github.com>
2025-11-08 13:29:54 +0800

    Tambah fungsi untuk tambah, edit, hapus ke database

--- Praktikum/Praktikum 9/app/models/SuperAdminModel.php
+++ Praktikum/Praktikum 9/app/models/SuperAdminModel.php
@@ -12,5 +12,34 @@ class SuperAdmin extends Database {
         $result = $stmt->get_result();
         return $result->fetch_assoc();
     }
+
+    public function GetAllUsers() {
+        $sql = "SELECT * FROM users";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function tambahAkun($username, $password, $kategori) {
+        $sql = "INSERT into users (username, password, kategori) VALUES (?,?,?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("ssi", $username, $password, $kategori);
+        return $stmt->execute();
+    }
+
+    public function hapus($username){
+        $sql = "DELETE FROM users WHERE username = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("s", $username);
+        return $stmt->execute();
+    }
+
+    public function edit($usernameawal, $username, $password, $kategori){
+        $sql = "UPDATE users SET username = ?, password = ?, kategori = ? WHERE username = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("ssis", $username, $password, $kategori, $usernameawal);
+        return $stmt->execute();
+    }
 }
 ?>
